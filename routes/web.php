305e1a799592d520f73105069a8b87bd211f4b7b
Powersteel <huzaifa.10xdigital@gmail.com>
2026-06-03 11:33:49 +0500

    dsf

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/link-storage', function () {
+    Artisan::call('storage:link');
+    return 'Storage link created!';
+});
